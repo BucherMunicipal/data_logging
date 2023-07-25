9c8c778ff6c41501c17131aebbff03e09f69161d
Bouchra Mohamed Lemine <109071284+BouchraMedLemine@users.noreply.github.com>
2023-07-25 08:53:21 +0100

    Updated the DBC file name

--- find_serial_number/set_serial_number.py
+++ find_serial_number/set_serial_number.py
@@ -29,7 +29,7 @@ def set_serial_number(config_data):
 
 
     # Create a CAN message decoder using the DBC file.
-    db = cantools.database.load_file('/mnt/ssd-1/workspace/jetson-deepstream/bscripts/logging/logged_can_signals_16_02_2023.DBC')  
+    db = cantools.database.load_file('/mnt/ssd-1/workspace/jetson-deepstream/bscripts/logging/logged_can_signals_25_07_2023.DBC')  
 
     bus = Bus()
 
